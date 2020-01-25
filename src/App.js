f9f5454eee2dd45c3bd56b0ee001cd11df764033
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-25 19:30:43 +0000

    feat: apply components to root component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import GuessedWords from '../src/components/GuessedWords/GuessedWords';
+import Congrats from '../src/components/Congrats/Congrats';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App' data-test='component-app'>
+        <h1>Jotto</h1>
+        <Congrats success={true} data-test='component-congrats' />
+        <GuessedWords 
+          guessedWords={[{ guessedWord: 'sam', letterMatchCount: 3 }]}
+          data-test='component-guessed-words' />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { setup, findByTestAttr, checkProps } from './testHelpers';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  let wrapper;
+  let appComponent;
+  const defaultProps = {};
+
+  beforeEach(() => {
+    wrapper = setup(App, defaultProps);
+    appComponent = findByTestAttr(wrapper, 'component-app');
+  });
+
+  it('renders without crashing', () => {
+   expect(appComponent).toHaveLength(1);
+  });
+
+  it('renders a <Congrats /> component', () => {
+    const congratsComponent = findByTestAttr(wrapper, 'component-congrats');
+    expect(congratsComponent).toHaveLength(1);
+  });
+
+  it('renders a <GuessedWords /> component', () => {
+    const guessedWordsComponent = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(guessedWordsComponent).toHaveLength(1);
+  });
 });
