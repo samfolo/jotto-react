686036efe5e8b6c3d178dd6d23ba0d5e6bc36506
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-26 10:57:47 +0000

    feat: code for input form

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -3,23 +3,36 @@ import { connect } from 'react-redux';
 
 class Input extends React.Component {
   render() {
+    const contents = this.props.success ? null : (
+      <form className='form-inline'>
+        <input 
+          type='text' 
+          data-test='input-box' 
+          className='mb-2 mx-sm-3' 
+          placeholder='enter guess' />
+        <button 
+          type='submit' 
+          data-test='submit-button' 
+          className='btn btn-primary mb-2'>Submit</button>
+      </form>
+    );
+
     return (
       <div data-test='component-input' >
-        <button />
+        {contents}
       </div>
     )
   }
 };
 
-const mapStateToProps = (state, action) => {
+const mapStateToProps = ({ success }) => {
   return {
-
+    success,
   }
 };
 
 const mapDispatchToProps = (dispatch, action) => {
   return {
-
   }
 };
 
--- src/Input/Input.test.js
+++ src/Input/Input.test.js
@@ -2,25 +2,27 @@ import { connectedSetup, findByTestAttr, storeFactory } from '../testHelpers';
 import Input from './Input';
 
 describe('<Input />', () => {
+  let initialState = { success: false };
   let defaultProps = {};
   let wrapper;
   let inputComponent;
 
   beforeEach(() => {
-    wrapper = connectedSetup(Input, defaultProps);
+    wrapper = connectedSetup(Input, initialState);
     inputComponent = findByTestAttr(wrapper, 'component-input');
   });
 
   describe('when the word has not been guessed', () => {
     it('renders without crashing', () => {
-      console.log(wrapper.debug());
       expect(inputComponent).toHaveLength(1);
     });
     it('renders the input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox).toHaveLength(1);
     });
     it('renders the submit button', () => {
-
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton).toHaveLength(1);
     });
   });
   
